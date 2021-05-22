eb055a7af70479a4690b0ae1b6294620f3a8f414
Gayan Kalinga <bg15407@gmail.com>
2021-05-22 17:54:15 +0530

    Added overlay to the button with on appear
    This added custom animation struct
    with overlay which has on appear combine with repeat forever animation modifier

--- SwiftUI Animation/ContentView.swift
+++ SwiftUI Animation/ContentView.swift
@@ -12,15 +12,36 @@ struct ContentView: View {
     
     var body: some View {
         Button("Hit It"){
-            scaleCount += 1
+            //scaleCount += 1
         }
         .padding(50)
         .background((scaleCount.truncatingRemainder(dividingBy: 2)) == 0 ? Color.red : Color.blue)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(scaleCount)
-        .animation(.default)
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(scaleCount)
+                .opacity(Double(2 - scaleCount))
+                .animation(
+                    Animation.easeInOut(duration: 2)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear{
+            scaleCount = 2
+        }
+        //.scaleEffect(scaleCount)
+        //.animation(.default)
+        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
         //.blur(radius: (scaleCount - 1) * 3)
+        //.animation(.easeInOut(duration: 2))
+//        .animation(
+//            Animation.easeInOut(duration: 2)
+//                .repeatForever(autoreverses: true)
+//                //.repeatCount(2, autoreverses: true)
+//                //.delay(1)
+//        )
     }
 }
 
