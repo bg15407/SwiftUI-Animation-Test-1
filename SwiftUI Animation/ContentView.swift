f00a358ff1d17851710a747a876e40ce691ca688
Gayan Kalinga <bg15407@gmail.com>
2021-05-22 17:26:15 +0530

    Implicit Animation for button
    Created a button with simple animation which will use scaleEffect to scale & animate when scaling.

--- SwiftUI Animation/ContentView.swift
+++ SwiftUI Animation/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scaleCount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Hit It"){
+            scaleCount += 1
+        }
+        .padding(50)
+        .background((scaleCount.truncatingRemainder(dividingBy: 2)) == 0 ? Color.red : Color.blue)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(scaleCount)
+        .animation(.default)
+        //.blur(radius: (scaleCount - 1) * 3)
     }
 }
 
