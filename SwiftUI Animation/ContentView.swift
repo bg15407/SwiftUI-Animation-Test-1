c1e29308329324f8ae7d815424a53691663edf7b
Gayan Kalinga <bg15407@gmail.com>
2021-05-22 12:56:52 +0530

    Initial Commit

---
+++ SwiftUI Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Animation
+//
+//  Created by Gayan Kalinga on 5/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI Animation
+//
+//  Created by Gayan Kalinga on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
